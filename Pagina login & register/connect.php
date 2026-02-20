614ded81be0d76948288036a6ff639393d95ed6c
YgorDev <203471232+YgorCODE@users.noreply.github.com>
2026-02-19 23:57:01 -0300

    Change error message for DB connection failure
    Updated error message for database connection failure.

--- Pagina login & register/connect.php
+++ Pagina login & register/connect.php
@@ -6,6 +6,7 @@ $pass="";
 $db="login";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
-    echo "Failed to connect DB".$conn->connect_error;
+    echo "Falha ao conectar ao banco de dados: ".$conn->connect_error;
 }
+
 ?>
