ecfd9c32e2c1873417cae2dcf32fbc3908293207
YgorDev <203471232+YgorCODE@users.noreply.github.com>
2026-02-19 23:53:59 -0300

    Translate login and registration page to Portuguese
    Updated the language of the login and registration page from English to Portuguese.

--- Pagina login & register/index.php
+++ Pagina login & register/index.php
@@ -1,81 +1,82 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register & Login</title>
+    <title>Registro e Login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container" id="signup" style="display:none;">
-      <h1 class="form-title">Register</h1>
+      <h1 class="form-title">Registrar</h1>
       <form method="post" action="register.php">
         <div class="input-group">
            <i class="fas fa-user"></i>
            <input type="text" name="fName" id="fName" placeholder="First Name" required>
-           <label for="fname">First Name</label>
+           <label for="fname">Nome</label>
         </div>
         <div class="input-group">
             <i class="fas fa-user"></i>
             <input type="text" name="lName" id="lName" placeholder="Last Name" required>
-            <label for="lName">Last Name</label>
+            <label for="lName">Sobrenome</label>
         </div>
         <div class="input-group">
             <i class="fas fa-envelope"></i>
             <input type="email" name="email" id="email" placeholder="Email" required>
-            <label for="email">Email</label>
+            <label for="email">E-mail</label>
         </div>
         <div class="input-group">
             <i class="fas fa-lock"></i>
             <input type="password" name="password" id="password" placeholder="Password" required>
-            <label for="password">Password</label>
+            <label for="password">Senha</label>
         </div>
        <input type="submit" class="btn" value="Sign Up" name="signUp">
       </form>
       <p class="or">
-        ----------or--------
+        ----------ou--------
       </p>
       <div class="icons">
          <i class="fab fa-google"></i>
           <i class="fab fa-facebook"></i>
              </div>
       <div class="links">
-        <p>Already Have Account ?</p>
-        <button id="signInButton">Sign In</button>
+        <p>Já possui uma conta ?</p>
+        <button id="signInButton">Entrar</button>
       </div>
     </div>
 
     <div class="container" id="signIn">
-        <h1 class="form-title">Sign In</h1>
+        <h1 class="form-title">Entrar</h1>
         <form method="post" action="register.php">
           <div class="input-group">
               <i class="fas fa-envelope"></i>
               <input type="email" name="email" id="email" placeholder="Email" required>
-              <label for="email">Email</label>
+              <label for="email">E-mail</label>
           </div>
           <div class="input-group">
               <i class="fas fa-lock"></i>
               <input type="password" name="password" id="password" placeholder="Password" required>
-              <label for="password">Password</label>
+              <label for="password">Senha</label>
           </div>
           <p class="recover">
-            <a href="#">Recover Password</a>
+            <a href="#">Recuperar Senha</a>
           </p>
          <input type="submit" class="btn" value="Sign In" name="signIn">
         </form>
         <p class="or">
-          ----------or--------
+          ----------ou--------
         </p>
         <div class="icons">
           <i class="fab fa-google"></i>
           <i class="fab fa-facebook"></i>
         </div>
         <div class="links">
-          <p>Don't have account yet?</p>
-          <button id="signUpButton">Sign Up</button>
+          <p>Ainda não possui uma conta?</p>
+          <button id="signUpButton">Registrar</button>
         </div>
       </div>
       <script src="script.js"></script>
 </body>
 </html>
+
